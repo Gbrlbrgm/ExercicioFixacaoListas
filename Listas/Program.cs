20770ae6ff4a547cdcf6cdedfa46eb4a37313ea9
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2021-04-02 17:31:26 -0300

    Projeto Concluído

---
+++ Listas/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered?");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i=1; i<=n; i++)
+            {
+                Console.WriteLine("Employee #" + i + ": ");
+                Console.WriteLine("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine("--------------------------");
+            }
+
+            Console.WriteLine("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.WriteLine("Enter the percentage");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This ID does not exist!");
+            }
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            
+        }
+    }
+}
